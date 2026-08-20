2257a2b491d2d540652ff161d8ea5e0774438aa7
Angel Felipe Cano Perilla <canos184@gmail.com>
2026-08-20 12:00:54 -0500

    feat(bank-templates): public exports

--- packages/bank-templates/src/index.ts
+++ packages/bank-templates/src/index.ts
@@ -1,1 +1,7 @@
-export {};
+export { matchTemplate } from "./engine/matchTemplate.js";
+export { extractFields } from "./engine/extractFields.js";
+export type { ExtractedFields } from "./engine/extractFields.js";
+export { normalizeAmount } from "./engine/normalizeAmount.js";
+export { normalizeDate } from "./engine/normalizeDate.js";
+export { templates, bancolombiaTemplate } from "./templates/index.js";
+export { bancolombiaCompraFixture } from "./fixtures/bancolombia-compra.js";
